15df842f35d2148f728e6dd3b8bf966c6918cc26
NolaZheng <mania001988@gmail.com>
2020-05-27 14:51:01 +0800

    deploy

---
+++ precache-manifest.8f6e95b91e2bb2ae01242b898ce5d6a3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165cbb98596611ae7b72",
+    "url": "/ecommerce-fashion-vue/css/about.05dc5680.css"
+  },
+  {
+    "revision": "dd20d77914a4e956a318",
+    "url": "/ecommerce-fashion-vue/css/app.9efb8cc1.css"
+  },
+  {
+    "revision": "b3846e203c0c68db5379",
+    "url": "/ecommerce-fashion-vue/css/chunk-vendors.d42f456a.css"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ecommerce-fashion-vue/img/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/ecommerce-fashion-vue/img/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "dca795f49916e524b7a772a5ce7e6cb2",
+    "url": "/ecommerce-fashion-vue/index.html"
+  },
+  {
+    "revision": "165cbb98596611ae7b72",
+    "url": "/ecommerce-fashion-vue/js/about.04d0416e.js"
+  },
+  {
+    "revision": "dd20d77914a4e956a318",
+    "url": "/ecommerce-fashion-vue/js/app.4de56565.js"
+  },
+  {
+    "revision": "b3846e203c0c68db5379",
+    "url": "/ecommerce-fashion-vue/js/chunk-vendors.e78b8afe.js"
+  },
+  {
+    "revision": "d606c6529683216a708f2401db28ce16",
+    "url": "/ecommerce-fashion-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ecommerce-fashion-vue/robots.txt"
+  }
+]);
